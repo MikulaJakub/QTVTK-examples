61d8211811dec98fe80f34588df91357a678cb10
MikulaJakub <mikula_jakub@centrum.sk>
2020-04-04 16:46:44 +0800

    cosmetics

---
+++ QTVTK-cylinder/build/src/CMakeFiles/test.out_autogen.dir/AutogenInfo.cmake
@@ -0,0 +1,39 @@
+# Meta
+set(AM_MULTI_CONFIG "SINGLE")
+# Directories and files
+set(AM_CMAKE_BINARY_DIR "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/build/")
+set(AM_CMAKE_SOURCE_DIR "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/")
+set(AM_CMAKE_CURRENT_SOURCE_DIR "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/src/")
+set(AM_CMAKE_CURRENT_BINARY_DIR "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/build/src/")
+set(AM_CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE "")
+set(AM_BUILD_DIR "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/build/src/test.out_autogen")
+set(AM_SOURCES "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/src/main.cpp;/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/src/mainwindow.cpp")
+set(AM_HEADERS "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/include/mainwindow.h")
+# Qt environment
+set(AM_QT_VERSION_MAJOR "5")
+set(AM_QT_VERSION_MINOR "9")
+set(AM_QT_MOC_EXECUTABLE "/opt/Qt5.9.0/5.9/gcc_64/bin/moc")
+set(AM_QT_UIC_EXECUTABLE "/opt/Qt5.9.0/5.9/gcc_64/bin/uic")
+set(AM_QT_RCC_EXECUTABLE "/opt/Qt5.9.0/5.9/gcc_64/bin/rcc")
+# MOC settings
+set(AM_MOC_SKIP "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/build/src/test.out_autogen/mocs_compilation.cpp")
+set(AM_MOC_DEFINITIONS "QT_CORE_LIB;QT_GUI_LIB;QT_NETWORK_LIB;QT_NO_DEBUG;QT_SQL_LIB;QT_WIDGETS_LIB")
+set(AM_MOC_INCLUDES "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/build/src/test.out_autogen/include;/opt/Qt5.9.0/5.9/gcc_64/include;/opt/Qt5.9.0/5.9/gcc_64/include/QtCore;/opt/Qt5.9.0/5.9/gcc_64/./mkspecs/linux-g++;/opt/Qt5.9.0/5.9/gcc_64/include/QtGui;/opt/Qt5.9.0/5.9/gcc_64/include/QtWidgets;/usr/include/x86_64-linux-gnu/qt5;/usr/include/x86_64-linux-gnu/qt5/QtNetwork;/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/include;/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/.;/usr/local/include/vtk-8.1;/usr/local/include;/opt/Qt5.9.0/5.9/gcc_64/include/QtSql;/usr/include")
+set(AM_MOC_OPTIONS "")
+set(AM_MOC_RELAXED_MODE "FALSE")
+set(AM_MOC_MACRO_NAMES "Q_OBJECT;Q_GADGET;Q_NAMESPACE")
+set(AM_MOC_DEPEND_FILTERS "")
+set(AM_MOC_PREDEFS_CMD "/usr/bin/c++;-dM;-E;-c;/usr/share/cmake-3.10/Modules/CMakeCXXCompilerABI.cpp")
+# UIC settings
+set(AM_UIC_SKIP "/home/jakub/Work/testing/QTVTK/QTVTK-cylinder/build/src/test.out_autogen/mocs_compilation.cpp")
+set(AM_UIC_TARGET_OPTIONS "")
+set(AM_UIC_OPTIONS_FILES "")
+set(AM_UIC_OPTIONS_OPTIONS "")
+set(AM_UIC_SEARCH_PATHS "")
+# RCC settings
+set(AM_RCC_SOURCES "")
+set(AM_RCC_BUILDS "")
+set(AM_RCC_OPTIONS "")
+set(AM_RCC_INPUTS "")
+# Configurations options
+set(AM_CONFIG_SUFFIX_ "_")
